2b60e214c7c418b5bc524a125946306833ee666a
rightfold <rightfold@gmail.com>
2017-05-13 02:22:18 +0200

    Select fresh topics automatically

--- src/Reiterate/UI.purs
+++ src/Reiterate/UI.purs
@@ -6,9 +6,11 @@ module Reiterate.UI
   , ui
   ) where
 
+import Control.Monad.Except.Trans (ExceptT)
 import Control.Monad.Free (Free)
 import Control.Monad.Trans.Class (lift)
-import Data.Lens ((.=))
+import Data.Lens (Lens', (.=), (?=), modifying)
+import Data.Lens.Record (prop)
 import Halogen.Component (Component, ComponentDSL, ComponentHTML, lifecycleComponent)
 import Halogen.HTML (HTML)
 import Halogen.HTML as H
@@ -17,7 +19,13 @@ import Reiterate.Topic (Topic, TopicID)
 import Reiterate.Topic.Algebra (Topics, freshTopic, getTopics)
 import Stuff
 
-type State = Error \/ List (TopicID /\ Topic)
+--------------------------------------------------------------------------------
+
+type State =
+  { errors :: List Error
+  , topics :: List (TopicID /\ Topic)
+  , selection :: Maybe TopicID
+  }
 data Query a
   = Refresh a
   | NewTopic a
@@ -25,11 +33,22 @@ type Input = Unit
 type Output = Void
 type Base = Free Topics
 
+stateErrors :: Lens' State (List Error)
+stateErrors = prop (SProxy :: SProxy "errors")
+
+stateTopics :: Lens' State (List (TopicID /\ Topic))
+stateTopics = prop (SProxy :: SProxy "topics")
+
+stateSelection :: Lens' State (Maybe TopicID)
+stateSelection = prop (SProxy :: SProxy "selection")
+
+--------------------------------------------------------------------------------
+
 ui :: Component HTML Query Input Output Base
 ui = lifecycleComponent {initialState, receiver, initializer, finalizer, eval, render}
 
 initialState :: Input -> State
-initialState = const \ Right $ Nil
+initialState = const $ {errors: Nil, topics: Nil, selection: Nothing}
 
 receiver :: Input -> Maybe (Query Unit)
 receiver = const Nothing
@@ -41,18 +60,19 @@ finalizer :: Maybe (Query Unit)
 finalizer = Nothing
 
 eval :: Query ~> ComponentDSL State Query Output Base
-eval (Refresh next) = do
-  topics <- lift \ unwrap $ getTopics
-  id .= topics
-  pure next
-eval (NewTopic next) = do
-  tid <- lift \ unwrap $ freshTopic
-  eval $ Refresh next
+eval (Refresh next) = next <$ try (stateTopics .= _) getTopics
+eval (NewTopic next) = try (stateSelection ?= _) freshTopic *> eval (Refresh next)
+
+try :: ∀ a q o m. Monad m => (a -> ComponentDSL State q o m Unit) ->
+  ExceptT Error m a -> ComponentDSL State q o m Unit
+try f = either (modifying stateErrors \ (:)) f <=< lift \ unwrap
 
 render :: State -> ComponentHTML Query
 render s =
   H.div []
     [ H.button [E.onClick \ E.input_ $ NewTopic]
         [H.text "New Topic"]
-    , H.text $ show s
+    , H.pre [] [H.text $ show s.errors]
+    , H.pre [] [H.text $ show s.topics]
+    , H.pre [] [H.text $ show s.selection]
     ]
